1770051b54563c8e6512253082f0c61aaa265fb7
K-kare <kamunawangari@gmail.com>
2024-05-20 11:51:19 -0700

    0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+gather employee data from API
+'''
+
+import re
+import requests
+import sys
+
+API_BASE = "https://jsonplaceholder.typicode.com"
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            id = int(sys.argv[1])
+            req = requests.get('{}/users/{}'.format(API_BASE, id)).json()
+            task = requests.get('{}/todos'.format(API_BASE)).json()
+            emp_name = req.get('name')
+            tasks = list(filter(lambda x: x.get('userId') == id, task))
+            finished_tasks = list(filter(lambda x: x.get('completed'), tasks))
+            print(
+                'Employee {} is done with tasks({}/{}):'.format(
+                    emp_name,
+                    len(finished_tasks),
+                    len(task)
+                )
+            )
+            if len(finished_tasks) > 0:
+                for task in finished_tasks:
+                    print('\t {}'.format(task.get('title')))
